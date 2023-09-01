ee02600f3f8cbc8cc86a1fa71417ddf18159d38a
Geo <dr-gudkov@mail.ru>
2023-09-01 18:31:51 +0300

    Дополнительная версия функции

--- Combi.py
+++ Combi.py
@@ -1,5 +1,10 @@
 from math import factorial
+import numpy as np
 
 
 def combinations(i, j):
     return int(factorial(i) / (factorial(j) * factorial(i - j)))
+
+
+def combination(n, k):
+    return np.math.factorial(n) // (np.math.factorial(k) * np.math.factorial(n - k))
